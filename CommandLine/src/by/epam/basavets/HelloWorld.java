9078ce406f59ae29042de8c8b6cd909d2378e357
Lehapuz <leshabosovec@gmail.com>
2022-03-12 15:41:47 +0300

    task 2.3

---
+++ CommandLine/src/by/epam/basavets/HelloWorld.java
@@ -0,0 +1,16 @@
+package by.epam.basavets;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class HelloWorld {
+
+public static void main(String[] args) {
+
+System.out.println("Hello мир");
+
+int t = StringUtils.indexOf("j", 1);
+
+System.out.println(t);
+
+}
+}
