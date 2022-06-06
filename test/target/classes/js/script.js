3c9bf96ee4029fda2853c6e25035a522bd64b25a
Lehapuz <89546432+Lehapuz@users.noreply.github.com>
2022-06-06 14:48:50 +0300

    task 10

---
+++ test/target/classes/js/script.js
@@ -0,0 +1,26 @@
+let name = 'vlad'
+var is = true
+console.log(is)
+
+
+$(document).on('click', '.post-link', function(){
+        var link = $(this);
+        var postId = link.data('id');
+        $.ajax({
+            method: "GET",
+            url: '/' + postId,
+            success: function(response)
+            {
+
+               var code = '<span>Автор:' + response.author + '</span>';
+                link.parent().append(code);
+            },
+            error: function(response)
+            {
+                if(response.status == 404) {
+                    alert('Пост не найдено!');
+                }
+            }
+        });
+        return false;
+    });
